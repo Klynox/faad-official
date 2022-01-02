06e4058c87d209a97a6834007c43526906391226
Confidence Yobo <confiyobo@gmail.com>
2022-01-02 07:13:26 -0600

    make slide infinite

--- pages/index.vue
+++ pages/index.vue
@@ -149,41 +149,7 @@
           FAAD oil powers some of the largest establishments in the country and
           proudly supports growing businesses
         </p>
-        <VueSlickCarousel
-          v-bind="slideSettings"
-          class="text-center partners-slide"
-        >
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/1.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/2.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/3.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/4.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/5.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/6.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/7.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/8.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/9.png" />
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <img src="@/static/images/partners/slides/10.png" />
-          </div>
-        </VueSlickCarousel>
+        <ParnershipSlide/>
       </div>
     </section>
     <section class="features">
@@ -261,51 +227,6 @@
     <Footer />
   </div>
 </template>
-
-<script>
-import VueSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
-
-export default {
-  components: { VueSlickCarousel },
-  data() {
-    return {
-      slideSettings: {
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 2,
-        responsive: [
-          {
-            breakpoint: 1200,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-            },
-          },
-        ],
-      },
-    };
-  },
-};
-</script>
 
 <style lang="scss">
 .call-to-action {
